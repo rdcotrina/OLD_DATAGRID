5a11aba03c26d10291a9fda2e7cedeee82d15c75
DAVID <DAVID@192.168.1.36>
2015-02-14 20:37:18 +0100

    septima clase

--- SP/dataGrid.sql
+++ SP/dataGrid.sql
@@ -5,20 +5,45 @@ USE `rrhh_`$$
 DROP PROCEDURE IF EXISTS `dataGrid`$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `dataGrid`(
-	IN _criterio VARCHAR(200)
+	IN _flag INT,
+	IN _criterio VARCHAR(200),
+	IN _pagina INT,
+	IN _reg_x_pag INT
     )
 BEGIN
+		
 	DECLARE search VARCHAR(200) DEFAULT '';
+	DECLARE _pag_actual INT;
+	
+
+	SET _pag_actual=(_pagina - 1) * _reg_x_pag;
 	
 	IF _criterio != '' THEN
 		SET search = CONCAT(' WHERE CONCAT(nombres," ",apellidos) LIKE "%',_criterio,'%" ');
 	END IF;
 	
 	SET @sentencia = CONCAT('
-	SELECT 
-		nombres,apellidos
-	FROM f_trabajador
+	SELECT  
+		COUNT(*) INTO @countx  
+	FROM f_trabajador 
 	',search,';
+	');
+	
+	PREPARE consulta FROM @sentencia;
+	EXECUTE consulta;
+	
+	DEALLOCATE PREPARE consulta;
+	SET @sentencia = NULL;
+	
+	
+	SET @sentencia = CONCAT('
+	SELECT  
+		nombres, 
+		apellidos, 
+		@countx AS total 
+	FROM f_trabajador 
+	',search,' 
+	LIMIT ',_pag_actual,',',_reg_x_pag,';
 	');
 	
 	PREPARE consulta FROM @sentencia;
