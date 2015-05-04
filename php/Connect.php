dde50415d7280ea35179a47fa871f25f5b01b4e0
rdcotrina <roger.cotrina.c@gmail.com>
2015-05-05 00:18:06 +0200

    IMPLEMETACION A SQL SERVER

--- php/Connect.php
+++ php/Connect.php
@@ -1,35 +1,32 @@
 <?php
-
-class Connect extends PDO{
+class Connect{
     
-    private $host = 'localhost';
+    private $host = 'RD\RDCC';
     private $port = '3306';
-    private $user = 'root';
-    private $pass = '';
-    private $motor = 'mysql';
-    private $dbname = 'rrhh_';
+    private $user = 'sa';
+    private $pass = '123';
+    private $motor = 'sql';
+    private $dbname = 'rr_';
     
     public function __construct() {
-        parent::__construct(
+        $conn = new PDO(
                 $this->getDns(), 
                 $this->user, 
-                $this->pass, 
-                array(
-                    PDO::ATTR_ERRMODE,
-                    PDO::ERRMODE_EXCEPTION,
-                    PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8'
-                )
+                $this->pass
         );
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        
+        return $conn;
     }
     
     private function getDns(){
         $dns = '';
         switch ($this->motor){
             case 'mysql':
-                $dns = 'mysql:host='.$this->host.';port='.$this->port.';dbname='.$this->dbname.';';
+                $dns = 'mysql:host='.$this->host.';port='.$this->port.';dbname='.$this->dbname;
                 break;
             case 'sql':
-                $dns = 'sqlsvr:server='.$this->host.';Database='.$this->dbname.';';
+                $dns = 'sqlsrv:Server='.$this->host.';Database='.$this->dbname;
                 break;
             case 'oracle':
                 $dns = 'oci:dbname='.$this->dbname;
--- php/ConsultarData.php
+++ php/ConsultarData.php
@@ -1,7 +1,13 @@
 <?php
 
-class ConsultarData{
+class ConsultarData extends Connect{
     
+    private $_DB;
+
+    public function __construct() {
+        $this->_DB = parent::__construct();
+    }
+
     public function getData($flag,$criterio=''){
         $DB = new Connect();
         
@@ -18,13 +24,27 @@ class ConsultarData{
         return $result;
     }
     
-    public function getDataSP($flag,$criterio='',$page,$regxpag){
+    public function __getDataSP($flag,$criterio='',$page,$regxpag){
         $DB = new Connect();
         
-        $sql = "CALL dataGrid('".$flag."','".$criterio."','".$page."','".$regxpag."')";
+        $sql = "CALL dataGrid('".$flag."','".$criterio."','".$page."','".$regxpag."'); ";
         
         $data = $DB->query($sql);
         $result = $data->fetchAll(PDO::FETCH_ASSOC);
+       
+        return $result;
+    }
+    
+    public function getDataSP($flag,$criterio='',$page,$regxpag){
+        
+        $sql = "EXEC dataGrid ?,?,?,? ";
+        
+        $data = $this->_DB->prepare($sql);
+        $data->execute(array($flag,$criterio,$page,$regxpag));
+        $data->nextRowset();
+        
+        $result = $data->fetchAll(PDO::FETCH_ASSOC);
+       
         return $result;
     }
     
